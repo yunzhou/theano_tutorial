2953459db4adb68bc846f58dd783462b7d573b9f
ZY0627 <ilikezhouyun@gmail.com>
2016-09-17 20:34:03 +1000

    Update check1.py
    make it compatible with python 3

--- check1.py
+++ check1.py
@@ -1,22 +1,22 @@
 from theano import function, config, shared, sandbox
 import theano.tensor as T
-import numpy
+import numpy as np
 import time
 
 vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
 iters = 1000
 
-rng = numpy.random.RandomState(22)
-x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
+rng = np.random.RandomState(22)
+x = shared(np.asarray(rng.rand(vlen), config.floatX))
 f = function([], T.exp(x))
-print f.maker.fgraph.toposort()
+print(f.maker.fgraph.toposort())
 t0 = time.time()
-for i in xrange(iters):
+for i in np.arange(iters):
     r = f()
 t1 = time.time()
-print 'Looping %d times took' % iters, t1 - t0, 'seconds'
-print 'Result is', r
-if numpy.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]):
-    print 'Used the cpu'
+print('Looping %d times took' % iters, t1 - t0, 'seconds')
+print('Result is', r)
+if np.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]):
+    print('Used the cpu')
 else:
-    print 'Used the gpu'
+    print('Used the gpu')
